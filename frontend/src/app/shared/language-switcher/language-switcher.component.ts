4f91867b30302a943ce5bc3441793b3fd2de5907
Juanjho120 <juan.jose120@hotmail.com>
2026-06-09 06:40:48 -0600

    fix: use Bootstrap JS for dropdowns

--- frontend/src/app/shared/language-switcher/language-switcher.component.ts
+++ frontend/src/app/shared/language-switcher/language-switcher.component.ts
@@ -1,12 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
 import { SupportedLanguage } from '../../core/i18n/language.model';
 import { LanguageService } from '../../core/i18n/language.service';
 
 @Component({
   selector: 'app-language-switcher',
   standalone: true,
-  imports: [TranslatePipe],
   templateUrl: './language-switcher.component.html'
 })
 export class LanguageSwitcherComponent {
